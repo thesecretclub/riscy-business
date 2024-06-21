70eff7f043de8e47cc7fcbf2d98fb2dd57acd7b2
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2024-06-22 01:51:39 +0200

    Implement basic control flow graph analysis

--- obfuscator/src/main.cpp
+++ obfuscator/src/main.cpp
@@ -132,125 +132,155 @@ static bool find_riscvm_run(const std::vector<uint8_t>& pe, uint64_t& address, s
     return false;
 }
 
-static bool
-disassemble_riscvm_run_analyze(Program& program, const uint64_t function_start, const std::vector<uint8_t>& code)
+static bool analyze_riscvm_run(Program& program)
 {
-    zasm::Decoder  decoder(program.getMode());
-    x86::Assembler assembler(program);
+    puts("=== ANALYZE ===");
+    std::vector<Label> queue;
+    queue.push_back(program.getEntryPoint());
+    std::set<Label::Id> visisted;
 
-    std::vector<uint64_t> queue;
-    queue.push_back(function_start);
-    std::map<uint64_t, Node*> nodes;
-    std::map<uint64_t, Label> labels;
-    std::set<uint64_t>        visisted;
+    // References:
+    // - https://en.wikipedia.org/wiki/Live-variable_analysis
+    // - https://en.wikipedia.org/wiki/Dominator_(graph_theory)
+
+    struct BasicBlock
+    {
+        uint64_t           address = 0;
+        Label              label;
+        Node*              begin = nullptr;
+        Node*              end   = nullptr;
+        std::vector<Label> successors;
+    };
+
+    std::map<uint64_t, BasicBlock> blocks;
 
     while (!queue.empty())
     {
-        auto block_start = queue.back();
+        auto block_start_label = queue.back();
         queue.pop_back();
 
-        if (visisted.count(block_start))
+        if (visisted.count(block_start_label.getId()))
         {
             continue;
         }
-        visisted.insert(block_start);
+        visisted.insert(block_start_label.getId());
 
-        printf("<==> Disassembling block: 0x%llX\n", block_start);
+        const auto& label_data    = *program.getLabelData(block_start_label);
+        auto        block_address = label_data.node->getUserDataU64();
 
-        uint64_t offset   = block_start - function_start;
-        bool     finished = false;
+        BasicBlock bb;
+        bb.address = block_address;
+        bb.label   = block_start_label;
+        bb.begin   = label_data.node->getNext();
+
+        printf("<==> Disassembling block: %s (0x%llX)\n", label_data.name, block_address);
+
+        uint32_t live_flags = 0;
+
+        auto format_flags = [](uint32_t mask)
+        {
+            std::string result;
+#define FLAG(x)   \
+    if (mask & x) \
+        result += (#x + 14), result += " ";
+            FLAG(ZYDIS_CPUFLAG_CF);
+            FLAG(ZYDIS_CPUFLAG_PF);
+            FLAG(ZYDIS_CPUFLAG_AF);
+            FLAG(ZYDIS_CPUFLAG_ZF);
+            FLAG(ZYDIS_CPUFLAG_SF);
+            FLAG(ZYDIS_CPUFLAG_TF);
+            FLAG(ZYDIS_CPUFLAG_IF);
+            FLAG(ZYDIS_CPUFLAG_DF);
+            FLAG(ZYDIS_CPUFLAG_OF);
+            FLAG(ZYDIS_CPUFLAG_IOPL);
+            FLAG(ZYDIS_CPUFLAG_NT);
+            FLAG(ZYDIS_CPUFLAG_RF);
+            FLAG(ZYDIS_CPUFLAG_VM);
+            FLAG(ZYDIS_CPUFLAG_AC);
+            FLAG(ZYDIS_CPUFLAG_VIF);
+            FLAG(ZYDIS_CPUFLAG_VIP);
+            FLAG(ZYDIS_CPUFLAG_ID);
+#undef FLAG
+            if (!result.empty())
+                result.pop_back();
+            return "(" + result + ")";
+        };
+
+        auto node     = label_data.node->getNext();
+        bool finished = false;
         while (!finished)
         {
-            if (offset >= code.size())
+            auto instr = node->getIf<Instruction>();
+            if (instr == nullptr)
             {
-                printf("offset out of bounds: 0x%llX\n", offset);
-                return false;
+                auto label = node->getIf<Label>();
+                queue.push_back(*label);
+                bb.successors.push_back(*label);
+                puts("not instr!");
+                break;
             }
 
-            uint8_t data[15] = {};
-            memcpy(data, code.data() + offset, sizeof(data));
+            auto user_data = node->getUserDataU64();
 
-            auto cur_address = function_start + offset;
-            auto decoder_res = decoder.decode(data, sizeof(data), cur_address);
-            if (!decoder_res)
+            auto str = formatter::toString(program, instr, formatter::Options::HexImmediates);
+            printf("0x%llX|%s|live=%s\n", user_data, str.c_str(), format_flags(live_flags).c_str());
+
+            // TODO: why isn't all this stored in the Instruction?
+            auto info = *instr->getDetail(program.getMode());
+
+            const auto& flags         = info.getCPUFlags();
+            auto        modified_mask = flags.set0 | flags.set1 | flags.modified | flags.undefined;
+            printf(
+                "\tset0: %s, set1: %s, modified: %s, undefined: %s\n",
+                format_flags(flags.set0.value()).c_str(),
+                format_flags(flags.set1.value()).c_str(),
+                format_flags(flags.modified.value()).c_str(),
+                format_flags(flags.undefined.value()).c_str()
+            );
+            printf("\tall_modified: %s\n", format_flags(modified_mask).c_str());
+            printf("\ttested: %s\n", format_flags(flags.tested.value()).c_str());
+
+            if (flags.tested.value() & live_flags)
             {
-                std::cout << "Failed to decode at " << std::hex << cur_address << ", "
-                          << decoder_res.error().getErrorName() << "\n";
-                return false;
+                printf("Live flags are tested: %s\n", format_flags(flags.tested.value() & live_flags).c_str());
             }
 
-            nodes.emplace(cur_address, assembler.getCursor());
-
-            const auto& info   = *decoder_res;
-            const auto  instr  = info.getInstruction();
-            auto        length = info.getLength();
-            offset += length;
-
-            auto str = formatter::toString(&instr, formatter::Options::HexImmediates);
-            printf("0x%llX|%s\n", cur_address, str.c_str());
-
-            auto emit = [&]
+            if (modified_mask.value() != 0)
             {
-                if (auto res = assembler.emit(instr); res != zasm::ErrorCode::None)
-                {
-                    std::cout << "Failed to emit instruction " << std::hex << cur_address << ", "
-                              << res.getErrorName() << "\n";
-                    return false;
-                }
-                return true;
-            };
+                live_flags = modified_mask;
+            }
 
             switch (info.getCategory())
             {
             case x86::Category::UncondBR:
             {
-                auto dest = info.getOperand<Imm>(0).value<uint64_t>();
-                printf("UncondBR: 0x%llX\n", dest);
+                auto dest = instr->getOperand<Label>(0);
+                printf("UncondBR: %d\n", dest.getId());
                 queue.push_back(dest);
-                auto itr = labels.find(dest);
-                if (itr == labels.end())
-                {
-                    char name[64] = "";
-                    sprintf_s(name, "label_%llX", dest);
-                    auto label = assembler.createLabel(name);
-                    itr        = labels.emplace(dest, label).first;
-                }
-                assembler.emit(info.getMnemonic(), itr->second);
+                bb.successors.push_back(dest);
                 finished = true;
             }
             break;
 
             case x86::Category::CondBr:
             {
-                auto brtrue  = info.getOperand<Imm>(0).value<uint64_t>();
-                auto brfalse = offset + function_start;
-                printf("CondBr: 0x%llX, 0x%llX\n", brtrue, brfalse);
+                auto brtrue  = instr->getOperand<Label>(0);
+                auto brfalse = node->getNext()->get<Label>();
+                printf("CondBr: %d, %d\n", brtrue.getId(), brfalse.getId());
                 queue.push_back(brfalse);
                 queue.push_back(brtrue);
-                auto itr = labels.find(brtrue);
-                if (itr == labels.end())
-                {
-                    char name[64] = "";
-                    sprintf_s(name, "label_%llX", brtrue);
-                    auto label = assembler.createLabel(name);
-                    itr        = labels.emplace(brtrue, label).first;
-                }
-                assembler.emit(info.getMnemonic(), itr->second);
+                bb.successors.push_back(brtrue);
+                bb.successors.push_back(brfalse);
                 finished = true;
             }
             break;
 
             case x86::Category::Call:
             {
-                auto dest = info.getOperand(0);
-                if (dest.getIf<Imm>() != nullptr)
+                auto dest = instr->getOperand(0);
+                if (dest.getIf<Label>() != nullptr)
                 {
                     printf("unsupported call imm\n");
-                    return false;
-                }
-
-                if (!emit())
-                {
                     return false;
                 }
             }
@@ -258,52 +288,71 @@ disassemble_riscvm_run_analyze(Program& program, const uint64_t function_start, 
 
             case x86::Category::Ret:
             {
-                if (!emit())
-                {
-                    return false;
-                }
                 finished = true;
             }
             break;
 
             default:
             {
-                if (!emit())
-                {
-                    return false;
-                }
+                // TODO: update flag liveness
             }
             break;
             }
+
+            node = node->getNext();
         }
+
+        bb.end = node;
+
+        blocks.emplace(bb.address, bb);
     }
 
-    for (const auto& [address, label] : labels)
+    std::map<uint64_t, std::set<uint64_t>> successors;
+    std::map<uint64_t, std::set<uint64_t>> predecessors;
+    for (const auto& [address, block] : blocks)
     {
-        auto node = nodes.at(address);
-        if (node == nullptr)
+        for (const auto& successor : block.successors)
         {
-            puts("oh nein");
-            __debugbreak();
+            auto successor_address = program.getLabelData(successor).value().node->getUserDataU64();
+            successors[address].insert(successor_address);
+            predecessors[successor_address].insert(address);
         }
-        assembler.setCursor(node);
-        assembler.bind(label);
     }
 
-    puts("");
-    std::string text = formatter::toString(program);
-    puts(text.c_str());
+    auto to_hex = [](uint64_t value)
+    {
+        char buffer[64] = "";
+        sprintf_s(buffer, "\"0x%llX\"", value);
+        return std::string(buffer);
+    };
+
+    std::string dot = "digraph G {\n";
+    for (const auto& [address, block] : blocks)
+    {
+        dot += to_hex(address) + " [label=\"" + program.getLabelData(block.label).value().name + "\"];\n";
+        for (const auto& successor : block.successors)
+        {
+            auto successor_address = program.getLabelData(successor).value().node->getUserDataU64();
+            dot += to_hex(address) + " -> " + to_hex(successor_address) + ";\n";
+        }
+    }
+
+    dot += "}";
+
+    puts(dot.c_str());
 
     return true;
 }
 
 static bool disassemble_riscvm_run(Program& program, const uint64_t function_start, const std::vector<uint8_t>& code)
 {
+    puts("=== DISASSEMBLE ===");
     zasm::Decoder  decoder(program.getMode());
     x86::Assembler assembler(program);
 
     auto entry_label = assembler.createLabel("riscvm_run");
     assembler.bind(entry_label);
+    assembler.getCursor()->setUserData(function_start);
     program.setEntryPoint(entry_label);
 
     std::map<uint64_t, Node*> nodes;
@@ -339,15 +388,12 @@ static bool disassemble_riscvm_run(Program& program, const uint64_t function_sta
                           << res.getErrorName() << "\n";
                 return false;
             }
+            assembler.getCursor()->setUserData(cur_address);
             return true;
         };
 
-        switch (info.getCategory())
+        auto create_label = [&](uint64_t dest)
         {
-        case x86::Category::UncondBR:
-        {
-            auto dest = info.getOperand<Imm>(0).value<uint64_t>();
-            printf("UncondBR: 0x%llX\n", dest);
             auto itr = labels.find(dest);
             if (itr == labels.end())
             {
@@ -356,7 +402,17 @@ static bool disassemble_riscvm_run(Program& program, const uint64_t function_sta
                 auto label = assembler.createLabel(name);
                 itr        = labels.emplace(dest, label).first;
             }
-            assembler.emit(info.getMnemonic(), itr->second);
+            return itr->second;
+        };
+
+        switch (info.getCategory())
+        {
+        case x86::Category::UncondBR:
+        {
+            auto dest = info.getOperand<Imm>(0).value<uint64_t>();
+            printf("UncondBR: 0x%llX\n", dest);
+            assembler.emit(info.getMnemonic(), create_label(dest));
+            assembler.getCursor()->setUserData(cur_address);
         }
         break;
 
@@ -364,16 +420,10 @@ static bool disassemble_riscvm_run(Program& program, const uint64_t function_sta
         {
             auto brtrue  = info.getOperand<Imm>(0).value<uint64_t>();
             auto brfalse = offset + function_start;
+            create_label(brfalse);
             printf("CondBr: 0x%llX, 0x%llX\n", brtrue, brfalse);
-            auto itr = labels.find(brtrue);
-            if (itr == labels.end())
-            {
-                char name[64] = "";
-                sprintf_s(name, "label_%llX", brtrue);
-                auto label = assembler.createLabel(name);
-                itr        = labels.emplace(brtrue, label).first;
-            }
-            assembler.emit(info.getMnemonic(), itr->second);
+            assembler.emit(info.getMnemonic(), create_label(brtrue));
+            assembler.getCursor()->setUserData(cur_address);
         }
         break;
 
@@ -418,6 +468,7 @@ static bool disassemble_riscvm_run(Program& program, const uint64_t function_sta
         auto node = nodes.at(address);
         assembler.setCursor(node);
         assembler.bind(label);
+        assembler.getCursor()->setUserData(address);
     }
 
     puts("");
@@ -429,9 +480,16 @@ static bool disassemble_riscvm_run(Program& program, const uint64_t function_sta
 
 static bool obfuscate_riscvm_run(Program& program)
 {
+    if (!analyze_riscvm_run(program))
+    {
+        return false;
+    }
+
+    return true;
+
     x86::Assembler assembler(program);
 
-    puts("OBFUSCATE");
+    puts("=== OBFUSCATE === ");
     auto entry_node = program.getLabelData(program.getEntryPoint()).value().node;
     for (auto node = entry_node; node != nullptr;)
     {
